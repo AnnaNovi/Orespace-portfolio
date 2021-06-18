6308ee8b67ccf4c48a8ea790f538b4d57d32b9f4
Anna <annprogram-gh@mail.ru>
2021-06-18 15:03:24 +0300

    finished with layout, going to add images

--- resrc/features.js
+++ resrc/features.js
@@ -1,6 +1,14 @@
 var swiper = new Swiper(".swiper-block", {
     pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable: true
+    },
+    initialSlide: 1
+});
+
+var swiper = new Swiper(".swiper-review", {
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true
     },
 });
