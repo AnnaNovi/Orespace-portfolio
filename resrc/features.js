f39d49c0689c4df152c95a8e80b18640e1d6e991
Anna <annprogram-gh@mail.ru>
2021-07-19 12:28:13 +0300

    finished with adaptability

--- resrc/features.js
+++ resrc/features.js
@@ -12,3 +12,37 @@ var swiper = new Swiper(".swiper-review", {
     clickable: true
     },
 });
+
+function headerSize(){
+  if ($(window).width() <= '650'){
+      $('.signed').appendTo('.header__menu');
+  } else {
+    $('.signed').appendTo('nav');
+  }
+}
+$(window).on('load resize',headerSize);
+
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu, .signed').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+function footerSize(){
+  if ($(window).width() <= '650'){
+      $('.sup-column').appendTo('.serv-column');
+  } else {
+    $('.sup-column').appendTo('footer .container');
+  }
+}
+$(window).on('load resize',footerSize);
+
+function footerlastSize(){
+  if ($(window).width() <= '450'){
+      $('.soc-column').appendTo('.long-info-column');
+  } else {
+    $('.soc-column').appendTo('footer .container');
+  }
+}
+$(window).on('load resize',footerlastSize);
